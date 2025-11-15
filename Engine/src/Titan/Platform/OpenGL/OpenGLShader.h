21f197eb5b4fed6ffd2da9ae07079a4dcadad653
TerraCraftere3 <terracraftere3@gmail.com>
2025-11-15 07:41:46 +0100

    Support for Geometry Shader

--- Engine/src/Titan/Platform/OpenGL/OpenGLShader.h
+++ Engine/src/Titan/Platform/OpenGL/OpenGLShader.h
@@ -1,36 +1,21 @@
 #pragma once
-
 #include <slang-com-ptr.h>
 #include <slang.h>
 #include <glm/glm.hpp>
 #include "Titan/Renderer/Shader.h"
-// clang-format off
-#ifdef APIENTRY
-    #undef APIENTRY
-#endif
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-// clang-format on
-namespace slang
-{
-    struct ISession;
-    struct IModule;
-} // namespace slang
 
-namespace Slang
-{
-    template <typename T>
-    class ComPtr;
-}
+typedef unsigned int GLenum;
+typedef int GLint;
 
 namespace Titan
 {
-
-    class TI_API OpenGLShader : public Shader
+    class OpenGLShader : public Shader
     {
     public:
         OpenGLShader(const std::string& filepath);
         OpenGLShader(const std::string& name, const std::string& vertexSrc, const std::string& fragmentSrc);
+        OpenGLShader(const std::string& name, const std::string& vertexSrc, const std::string& geometrySrc,
+                     const std::string& fragmentSrc);
         virtual ~OpenGLShader();
 
         virtual void Bind() const override;
@@ -39,30 +24,27 @@ namespace Titan
         virtual void SetBool(const std::string& name, bool value) override;
         virtual void SetInt(const std::string& name, int value) override;
         virtual void SetIntArray(const std::string& name, int* values, uint32_t count) override;
-
         virtual void SetFloat(const std::string& name, float value) override;
         virtual void SetFloat2(const std::string& name, const glm::vec2& value) override;
         virtual void SetFloat3(const std::string& name, const glm::vec3& value) override;
         virtual void SetFloat4(const std::string& name, const glm::vec4& value) override;
-
         virtual void SetMat2(const std::string& name, const glm::mat2& value) override;
         virtual void SetMat3(const std::string& name, const glm::mat3& value) override;
         virtual void SetMat4(const std::string& name, const glm::mat4& value) override;
 
-        virtual const std::string& GetName() const override { return m_Name; };
+        virtual const std::string& GetName() const override { return m_Name; }
 
     private:
-        GLint GetUniformLocation(const std::string& name);
+        std::unordered_map<GLenum, std::string> ParseShaderFile(const std::string& source);
         void Compile(const std::unordered_map<GLenum, std::string>& shaderSources);
         void CompileSlangShader(const std::string& filepath);
         std::string CompileSlangEntryPoint(Slang::ComPtr<slang::ISession> session, slang::IModule* module,
                                            Slang::ComPtr<slang::IEntryPoint> entryPoint,
                                            const std::string& entryPointName);
-        std::unordered_map<GLenum, std::string> ParseShaderFile(const std::string& source);
+        GLint GetUniformLocation(const std::string& name);
 
     private:
-        std::string m_Name;
         uint32_t m_RendererID;
+        std::string m_Name;
     };
-
 } // namespace Titan
