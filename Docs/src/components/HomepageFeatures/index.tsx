8de6a2a5106bd353515bb09e7a73661ff9ac17ca
TerraCraftere3 <terracraftere3@gmail.com>
2026-01-06 20:09:48 +0100

    Using 'useBaseUrl' in Documentation Homepage

--- Docs/src/components/HomepageFeatures/index.tsx
+++ Docs/src/components/HomepageFeatures/index.tsx
@@ -3,6 +3,7 @@ import clsx from 'clsx';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
 import React from 'react';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
 type FeatureItem = {
   title: string;
@@ -15,39 +16,29 @@ type FeatureItem = {
 const FeatureList: FeatureItem[] = [
   {
     title: 'PBR Rendering',
-    Image: require('@site/static/img/screenshots/helmet.png').default,
-    description: (
-      <>
-        Get stunning visuals with Titan Engine's modern Physically Based Rendering (PBR) system
-      </>
-    ),
+    Image: 'img/screenshots/helmet.png',
+    description: <>Get stunning visuals with Titan Engine's modern Physically Based Rendering (PBR) system</>,
   },
   {
     title: 'Prebuilt Editor',
-    Image: require('@site/static/img/screenshots/editor.png').default,
-    description: (
-      <>
-        An easy to use prebuilt editor to help you build your games faster. (although you can also use the engine without it!)
-      </>
-    ),
+    Image: 'img/screenshots/editor.png',
+    description: <>An easy to use prebuilt editor to help you build your games faster. (although you can also use the engine without it!)</>,
   },
   {
     title: 'Physics',
-    Image: require('@site/static/img/screenshots/physics.png').default,
-    description: (
-      <>
-        Use PhysX and Box2D to simulate realtime physics in your games, from rigid bodies to complex collisions.
-      </>
-    ),
+    Image: 'img/screenshots/physics.png',
+    description: <>Use PhysX and Box2D to simulate realtime physics in your games, from rigid bodies to complex collisions.</>,
   },
 ];
 
+
 function Feature({ title, Svg, Image, description }: FeatureItem) {
+  const imgUrl = Image ? useBaseUrl(Image) : undefined; // ensures baseUrl is prepended
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
         {Svg && <Svg className={styles.featureSvg} role="img" />}
-        {Image && <img src={Image} className={styles.featureImage} role="img" />}
+        {imgUrl && <img src={imgUrl} className={styles.featureImage} role="img" />}
       </div>
       <div className="text--center padding-horiz--md">
         <Heading as="h3">{title}</Heading>
@@ -56,6 +47,7 @@ function Feature({ title, Svg, Image, description }: FeatureItem) {
     </div>
   );
 }
+
 
 export default function HomepageFeatures(): ReactNode {
   return (
