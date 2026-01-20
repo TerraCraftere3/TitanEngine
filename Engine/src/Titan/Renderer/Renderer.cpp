f1c067790023300b69127a71cb94d1be307975c1
TerraCraftere3 <terracraftere3@gmail.com>
2026-01-20 19:36:06 +0100

    Removed Legacy Renderer Code

--- Engine/src/Titan/Renderer/Renderer.cpp
+++ Engine/src/Titan/Renderer/Renderer.cpp
@@ -34,29 +34,6 @@ namespace Titan
         FullscreenRenderer::Shutdown();
     }
 
-    void Renderer::BeginScene(Camera& camera, const glm::mat4& transform)
-    {
-        s_SceneData->ViewProjMatrix = camera.GetProjection() * glm::inverse(transform);
-    }
-
-    void Renderer::EndScene() {}
-
-    void Renderer::Submit(const Ref<VertexArray>& vertexArray, const Ref<Shader>& shader, const glm::mat4& transform)
-    {
-        // Create a temporary pipeline state for this legacy submit call
-        Ref<PipelineState> pipeline = PipelineState::Create();
-        pipeline->SetShader(shader);
-        pipeline->SetVertexArray(vertexArray);
-        pipeline->Bind();
-
-        // Set uniforms on shader directly
-        std::dynamic_pointer_cast<OpenGLShader>(shader)->SetMat4("u_ViewProjection",
-                                                                 s_SceneData->ViewProjMatrix); // LEGACY METHODS
-        std::dynamic_pointer_cast<OpenGLShader>(shader)->SetMat4("u_Model", transform);        // TODO: REMOVE!!!
-
-        RenderCommand::DrawIndexed();
-    }
-
     void Renderer::OnWindowResize(uint32_t width, uint32_t height)
     {
         RenderCommand::SetViewport(0, 0, width, height);
